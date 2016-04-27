cede372127da3a6788fe66aba399f3ff0ba900d0
Kiran Bangale <kiran.bangale@cuelogic.co.in>
2016-04-27 11:56:28 +0530

    Updates on Day4 Training

--- app/modules/home/home.controller.js
+++ app/modules/home/home.controller.js
@@ -10,11 +10,18 @@ function HomeController($scope, $localStorage, $state, $filter) {
     $scope.employees = $localStorage.empList;
     $scope.sortOpts = ['name', 'email', 'age', 'gender'];
 
+    $scope.redirect = redirect;
     $scope.order = order;
 
     $scope.$on('update-empList', function(event, data) {
         $scope.employees = data;
     });
+
+    ui - sref = "edit({id: '{{data._id}}'})"
+
+    function redirect(id) {
+        $state.go('edit')
+    };
 
     function order(predicate) {
         $scope.predicate = predicate;
